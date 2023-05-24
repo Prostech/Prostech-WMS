11a24f26f6d54df4d097c71db7715079a82dd793
Tan Phat Vo <recca5p@gmail.com>
2023-05-24 15:47:09 +0700

    build the layer for the project

---
+++ Prostech.WMS.Website/app/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useEffect, useState } from "react";
+
+//form : use-hook-form  && validate field : yup
+
+//
+
+export default function Home() {
+  const [loading, SetLoading] = useState(false);
+
+  const HandleCick = (load: any) => {
+    SetLoading(!load);
+  };
+  useEffect(() => {
+    console.log(loading);
+  }, [loading]);
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <div>Home</div>
+      <button onClick={() => HandleCick(loading)}>Click</button>
+    </main>
+  );
+}
