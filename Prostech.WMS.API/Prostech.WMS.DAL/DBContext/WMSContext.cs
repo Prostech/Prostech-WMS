a89e5fca3eb87b334820e7aa78fa25eedb887779
Tan Phat Vo <recca5p@gmail.com>
2023-06-14 17:17:34 +0700

    delete db connection

--- Prostech.WMS.API/Prostech.WMS.DAL/DBContext/WMSContext.cs
+++ Prostech.WMS.API/Prostech.WMS.DAL/DBContext/WMSContext.cs
@@ -29,7 +29,7 @@ namespace Prostech.WMS.DAL.DBContext
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("User Id=admin;Password=Rozitek123@#;Host=localhost;Port=5432;Database=wms;");
+            optionsBuilder.UseNpgsql("");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
