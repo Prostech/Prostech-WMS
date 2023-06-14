4a20549afc6e016ec0388a5ea8e2293f991cd06a
Tan Phat Vo <recca5p@gmail.com>
2023-06-14 17:27:07 +0700

    remove db config in db context

--- Prostech.WMS.API/Prostech.WMS.DAL/DBContext/WMSContext.cs
+++ Prostech.WMS.API/Prostech.WMS.DAL/DBContext/WMSContext.cs
@@ -27,10 +27,10 @@ namespace Prostech.WMS.DAL.DBContext
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseNpgsql("");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseNpgsql("");
+        //}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
