5e848a98811a6e5bd81988c00743bb8c6e8d19ca
Tan Phat Vo <recca5p@gmail.com>
2023-06-15 12:43:56 +0700

    push the timehelper

---
+++ Prostech.WMS.API/Prostech.WMS.BLL/Helpers/Time/TimeHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prostech.WMS.BLL.Helpers.Time
+{
+    public static class TimeHelper
+    {
+        public static DateTime CurrentTime
+        {
+            get
+            {
+                DateTime utcTime = DateTime.UtcNow;
+                TimeZoneInfo cstZone = TimeZoneInfo.FindSystemTimeZoneById("SE Asia Standard Time");
+                DateTime cstTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, cstZone);
+                return cstTime;
+            }
+        }
+    }
+
+}
